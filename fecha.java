88050c8860176d6e3c404aa62c8cf0b879746151
Y0SDan <73656015+Y0SDan@users.noreply.github.com>
2022-11-02 18:11:51 -0600

    agregué la funcion que cuenta los dias transcurridos

--- fecha.java
+++ fecha.java
@@ -59,6 +59,46 @@ public class Fecha{
     }
   }
 
+  public void yearDays(){
+    int i = 0;
+
+    while(i <= 12){
+      if(i != month){
+        switch(month){
+          case 1:
+            day = day + 31; break;
+          case 2:
+            if(leapYear()){
+              day = day + 29; break;
+            }else{
+                day = day + 28; break;
+            }
+          case 3:
+            day = day + 31; break;
+          case 4:
+            day = day + 30; break;
+          case 5:
+            day = day + 31; break;
+          case 6:
+            day = day + 30; break;
+          case 7:
+            day = day + 31; break;
+          case 8:
+            day = day + 31; break;
+          case 9:
+            day = day + 30; break;
+          case 10:
+            day = day + 31; break;
+          case 11:
+            day = day + 30; break;
+          case 12:
+            day = day + 31; break;
+        }
+      }
+      i++;
+    }
+  }
+
   public void nexDay(){
     day++;
 
@@ -73,7 +113,33 @@ public class Fecha{
   }
 
   public void days(){
-
+    if(leapYear()){
+      switch(month){
+        case 2:
+          day = 29 - day; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+          day = 30 - day; break;
+        default:
+          day = 31 - day; break;
+      }
+      yearDays();
+    }else{
+      switch(month){
+        case 2:
+          day = 28 - day; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+          day = 30 - day; break;
+        default:
+          day = 31 - day; break;
+      }
+      yearDays();
+    }
   }
 
 }
